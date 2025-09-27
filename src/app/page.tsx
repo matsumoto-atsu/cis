5fb01aab0eabd558bb31175a84f9454471296dfe
matsumoto-atsu <kunitachi0316@gmail.com>
2025-09-27 17:19:29 +0900

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main style={{padding: 24, fontFamily: 'system-ui'}}>
       <h1>Hello World 🎉</h1>
+      <h1>こっからサイトをつくってくぞ</h1>
       <p>Next.js on Vercel</p>
     </main>
   );
